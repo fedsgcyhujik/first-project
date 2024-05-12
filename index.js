be0c6056eeabba3b14bfa4101c6eec8cbeab9dca
fedsgcyhujik <lsjcuizjoaquim2@hotmail.com>
2024-05-12 23:07:41 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
